fca99edd2bd99bbee19587707aa8fd7f91c8f0e0
Toyeeb Ganiu <tboynonilekan@gmail.com>
2020-06-15 20:50:43 +0100

    fixed instructions page

--- server/libs.js
+++ server/libs.js
@@ -530,7 +530,7 @@ module.exports = {
     });
   },
   handleRequest(request, response, url, fs, clientP) {
-    response.writeHead(200, { "Content-Type": "text/html" });
+    // response.writeHead(200, { "Content-Type": "text/html" });
 
     var path = url.parse(request.url).pathname;
     switch (path) {
